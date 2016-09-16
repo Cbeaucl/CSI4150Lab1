ec52ff101de650385cea7c9333e9bdac4d682abe
Chris Beauclair <chris.beauclair@gmail.com>
2016-09-16 10:33:29 -0400

    Working KeyGeneration

--- src/crypto/CryptoUtilsTest.java
+++ src/crypto/CryptoUtilsTest.java
@@ -1,6 +1,11 @@
 package crypto;
 
 import java.io.File;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.SecureRandom;
 
 /**
  * A tester for the CryptoUtils class.
@@ -8,7 +13,10 @@ import java.io.File;
  *
  */
 public class CryptoUtilsTest {
-    public static void main(String[] args) {
+    public void main(String[] args) {
+    	KeyPair cryptoKeys = generateKeyPair();
+    	cryptoKeys.getPrivate();
+    	KeyPair signingKeys = generateKeyPair();
         String key = "Mary has one cat";
         File inputFile = new File("PlainText.txt");
         File encryptedFile = new File("PlainText.encrypted");
@@ -21,5 +29,20 @@ public class CryptoUtilsTest {
             System.out.println(ex.getMessage());
             ex.printStackTrace();
         }
+    }
+    
+    public KeyPair generateKeyPair()
+    {
+    	KeyPairGenerator generator = null;
+    	try {
+			generator = KeyPairGenerator.getInstance("DSA", "SUN");
+			SecureRandom random = SecureRandom.getInstance("SHA1PRNG", "SUN");
+			generator.initialize(1024, random);
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		} catch (NoSuchProviderException e) {
+			e.printStackTrace();
+		}
+    	return generator.generateKeyPair();
     }
 }
